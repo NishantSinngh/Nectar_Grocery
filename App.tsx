13bf55cada790fa919aa2dd5f5fc8e4abb6bcbf0
Nishant Singh <nishantsingh9900@gmail.com>
2025-04-09 19:24:27 +0530

    files added

--- App.tsx
+++ App.tsx
@@ -2,10 +2,9 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import SettingScreen from './src/Screens/SettingsScreen/SettingScreen'
 import { SettingsScreen } from './src/Screens'
-
 const App = () => {
   return (
-    <View style={{flex:1}}>
+    <View style={{ flex: 1 }}>
       <SettingsScreen />
     </View>
   )
