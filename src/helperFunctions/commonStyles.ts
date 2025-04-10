9f9e53b808a6211e0b846a5b0096757be5723d81
Nishant Singh <nishantsingh9900@gmail.com>
2025-04-10 17:36:29 +0530

    ui changes

--- src/helperFunctions/commonStyles.ts
+++ src/helperFunctions/commonStyles.ts
@@ -5,6 +5,7 @@ export default StyleSheet.create({
     appHeader: {
         paddingTop: 35,
         paddingBottom: 25,
-        elevation: 4,
+        borderBottomWidth: 1,
+        borderBottomColor: colors.grey1
     }
 })
