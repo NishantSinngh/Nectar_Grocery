766ee74a8e55649bcb6bd43b151d2cd21aea2b51
Nishant Singh <nishantsingh9900@gmail.com>
2025-04-21 18:34:32 +0530

    bootsplash fixes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { View, Text, SafeAreaView, StatusBar } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SettingScreen } from './src/Screens'
 import MainStack from './src/Navigation/MainStack'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
+import BootSplash from "react-native-bootsplash";
 const App = () => {
+
+  useEffect(()=>{
+    BootSplash.hide({ fade: true });
+  },[])
+
   return (
     <Provider store={store}>
       <MainStack />
--- src/Screens/HomeScreen/HomeScreen.tsx
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, Image, ScrollView, Text, View } from 'react-native'
+import { ActivityIndicator, Image, KeyboardAvoidingView, ScrollView, Text, View } from 'react-native'
 import React from 'react'
 import homeStyles from './homeStyles'
 import SearchBar from '../../components/SearchBar'
@@ -16,39 +16,40 @@ const HomeScreen = () => {
 
 
     return (
-
-        <ScrollView style={{ flex: 1, flexGrow: 1, }} keyboardDismissMode='on-drag' keyboardShouldPersistTaps='handled' showsVerticalScrollIndicator={false} >
-            <View style={homeStyles.appContainer}>
-                <View style={homeStyles.headerStyle}>
-                    <Image source={imagePath.carrot} />
-                    <View style={homeStyles.locationContainer}>
-                        <Image source={imagePath.location} />
-                        {loading ? <ActivityIndicator /> : <Text style={homeStyles.locationText}>Location : {currentPosition?.coords?.longitude}, {currentPosition?.coords?.latitude}</Text>}
+        <KeyboardAvoidingView style={{flex: 1, flexGrow: 1,}}>
+            <ScrollView style={{ flex: 1, flexGrow: 1, }} keyboardDismissMode='on-drag' keyboardShouldPersistTaps='handled' showsVerticalScrollIndicator={false} >
+                <View style={homeStyles.appContainer}>
+                    <View style={homeStyles.headerStyle}>
+                        <Image source={imagePath.carrot} />
+                        <View style={homeStyles.locationContainer}>
+                            <Image source={imagePath.location} />
+                            {loading ? <ActivityIndicator /> : <Text style={homeStyles.locationText}>Location : {currentPosition?.coords?.longitude}, {currentPosition?.coords?.latitude}</Text>}
+                        </View>
                     </View>
+                    <SearchBar />
+                    <View style={homeStyles.offerContainer} >
+                        <Text style={homeStyles.headingText}>Exclusive Offer</Text>
+                        {/* <Text style={homeStyles.seeAllText}>See all</Text> */}
+                    </View>
+                    <ExclusiveOfferList />
+                    <View style={homeStyles.offerContainer} >
+                        <Text style={homeStyles.headingText}>Best Selling</Text>
+                        {/* <Text style={homeStyles.seeAllText}>See all</Text> */}
+                    </View>
+                    <BestSellingList />
+                    <View style={homeStyles.offerContainer} >
+                        <Text style={homeStyles.headingText}>Groceries </Text>
+                        {/* <Text style={homeStyles.seeAllText}>See all</Text> */}
+                    </View>
+                    <ScrollView horizontal contentContainerStyle={{ flexGrow: 1, marginTop: 10, }} >
+                        <ImageButton imgSrc={imagePath.pulses} imgStyle={{ marginRight: 10, marginLeft: 20, }} />
+                        <ImageButton imgSrc={imagePath.pulses} imgStyle={{ marginRight: 10 }} />
+                    </ScrollView>
+                    <GroceriesList />
+                    <Spacer space={100} />
                 </View>
-                <SearchBar />
-                <View style={homeStyles.offerContainer} >
-                    <Text style={homeStyles.headingText}>Exclusive Offer</Text>
-                    {/* <Text style={homeStyles.seeAllText}>See all</Text> */}
-                </View>
-                <ExclusiveOfferList />
-                <View style={homeStyles.offerContainer} >
-                    <Text style={homeStyles.headingText}>Best Selling</Text>
-                    {/* <Text style={homeStyles.seeAllText}>See all</Text> */}
-                </View>
-                <BestSellingList />
-                <View style={homeStyles.offerContainer} >
-                    <Text style={homeStyles.headingText}>Groceries </Text>
-                    {/* <Text style={homeStyles.seeAllText}>See all</Text> */}
-                </View>
-                <ScrollView horizontal contentContainerStyle={{ flexGrow: 1,marginTop:10, }} >
-                    <ImageButton imgSrc={imagePath.pulses} imgStyle={{ marginRight: 10, marginLeft: 20, }} />
-                    <ImageButton imgSrc={imagePath.pulses} imgStyle={{ marginRight: 10 }} />
-                </ScrollView>
-                <GroceriesList />
-                <Spacer space={100} />
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
